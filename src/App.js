62e156c37c6ca34bf6298db3993b007eb933729c
mkmeden <midhupayathuparambil@gmail.com>
2024-06-06 00:02:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+
+import logo from './assets/logo.svg'
 import './App.css';
+import  star from './assets/Star.svg'
+import { motion } from "framer-motion"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <a href="">TEAM</a>
+        <a href="">CONTACT</a>
+      </nav>
+      <section className="hero" >
+        
+
+        <div className="content">
+        <img id= "logo" src={logo} alt="" />
+        <p>CRAFTING NEXT GENERATION INTERFACES</p>
+        <h1>APP DEVELOPMENT</h1>
+        </div>
+
+        <div className='stars'>
+        <motion.img  id='star1' src={star} alt="" 
+        initial={{ opacity: 0.5, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{repeat : Infinity , duration :1  ,repeatType: "mirror"}}/>
+        <motion.img  id='star2' src={star} alt="" 
+        initial={{ opacity: 0.5, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{repeat : Infinity , duration :1  ,repeatType: "mirror"}}/>
+        <motion.img  id='star3' src={star} alt="" 
+        initial={{ opacity: 0.5, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{repeat : Infinity , duration :1  ,repeatType: "mirror"}}/>
+        <motion.img  id='star4' src={star} alt="" 
+        initial={{ opacity: 0.5, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{repeat : Infinity , duration :1  ,repeatType: "mirror"}}/>
+
+        </div>
+
+
+      </section>
+      <h1>teams</h1>
+      <section className="teams">
+      </section>
+      <section className="contact">
+        <h1>contact</h1>
+      </section>
     </div>
   );
 }
