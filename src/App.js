f0d69475df91e33dff33e2ed09c1d301c400fbb1
IMRAN-SAJAWAL-1234 <imransajwal.k@gmail.com>
2024-06-12 18:51:56 +0530

    contact-page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import star from './assets/Star.svg';
 import { motion } from "framer-motion";
 import Hero from './components/Hero';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       </section>
       <section className="contact" id='contactid'>
       <h1>CONTACT</h1>
+      <Contact/>
       </section>
     </div>
   );
