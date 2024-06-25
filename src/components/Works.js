de989f615b8eba9736434fb7c5961534edea5ac5
mkmeden <midhupayathuparambil@gmail.com>
2024-06-25 18:22:52 +0530

    Slider complete

--- src/components/Works.js
+++ src/components/Works.js
@@ -4,7 +4,8 @@ import hand from '../assets/hand.svg';
 import neuroWeb from '../assets/neuro_web.jpeg';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-
+import { CustomLeftArrow, CustomRightArrow } from './CustomArrows';
+import phone from '../assets/phone.png';
 const responsive = {
   superLargeDesktop: {
     // the naming can be any, depends on you.
@@ -33,11 +34,17 @@ const Works = () => {
       <h1 className='worksText'>WORKS</h1>
 
       <div className='slider'>
-        <img src={hand} className='hand1' alt="" />
 
         <div className="container">
 
-          <Carousel swipeable={false} draggable = {false} containerClass="carousel-container" responsive={responsive}>
+          <Carousel
+          swipeable={false}
+          draggable = {false} 
+          containerClass="carousel-container" 
+          customLeftArrow={<CustomLeftArrow />}
+          customRightArrow={<CustomRightArrow />}
+          infinite={true}
+          responsive={responsive}>
 
             <div className='card'>
 
@@ -50,13 +57,22 @@ const Works = () => {
 
             </div>
 
-            <div>Item 2</div>
+            <div className='card'>
+
+              <img src={phone} alt="website" />
+
+              <div className='text'>
+                <h1>Neurotunez App(In progress)</h1>
+                <p>We are a team of developers and recent graduates from NIT Calicut. With a fresh perspective and a drive for innovation, we deliver trending and top-notch software solutions.</p>
+              </div>
+
+            </div>
             <div>Item 3</div>
             <div>Item 4</div>
-          </Carousel>;
+          </Carousel>
 
         </div>
-        <img src={hand} className='hand2' alt="" />
+
       </div>
 
     </section>
